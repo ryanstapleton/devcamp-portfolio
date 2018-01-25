44d6e628acae6d7289acaa7e0141cab03a94f86b
ryanstapleton <megalixir.1@gmail.com>
2018-01-25 14:44:24 -0700

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   # Custom scope the method way
@@ -13,7 +14,7 @@ class Portfolio < ApplicationRecord
 
   def set_defaults
       # Set values to these defaults only if the values are nil
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/250x150"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '250', width: '150')
   end
 end
