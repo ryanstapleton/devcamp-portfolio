7c7ba3ad68b7a035459c5b4e74c230d3832e588b
ryanstapleton <megalixir.1@gmail.com>
2018-01-26 15:59:33 -0700

    Completed authentication with virtual attrs for user or model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   # :omniauthable - rules about third party logins
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+  
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
