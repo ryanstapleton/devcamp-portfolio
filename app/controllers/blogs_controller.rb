d4eefeea630b42a211f993d0c745bec978d02aef
ryanstapleton <megalixir.1@gmail.com>
2018-02-03 15:16:57 -0700

    Updated auth to include draft and published status controls, and implemented index styles for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,13 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  # Petergate authorizations
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.order(created_at: :desc)
     @page_title = "My Portfolio Blog"
   end
 
