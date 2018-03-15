be61bd9980e66bdeab39d5e56324f1b80e6672dd
ryanstapleton <megalixir.1@gmail.com>
2018-03-15 09:53:06 -0600

    Test1 on ping code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,5 +22,9 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
-
 // restart the server after any changes
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://ryanstapleton.com");
+}, 300000); // every 5 minutes (300000)
--- app/assets/javascripts/ping.js
+++
@@ -1,4 +0,0 @@
-// var http = require("http");
-// setInterval(function() {
-//     http.get("http://ryanstapleton.com");
-// }, 300000); // every 5 minutes (300000)
