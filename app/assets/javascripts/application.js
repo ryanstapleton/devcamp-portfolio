d7a5f4f62e38297e88a19de46b2076463d1f90bd
ryanstapleton <megalixir.1@gmail.com>
2018-04-05 14:45:34 -0600

    Removed JS ping code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,8 +23,3 @@
 //= require bootstrap-sprockets
 
 // restart the server after any changes
-
-var http = require("http");
-setInterval(function() {
-    http.get("http://ryanstapleton.com");
-}, 300000); // every 5 minutes (300000)
